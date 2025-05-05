f4325060c42f418300b72a56c38b39ad780a54ca
fhdw-pr <philipp.renerig@fhdw.de>
2025-05-05 12:34:13 +0200

    Add Exercise 01

---
+++ src/exercises/Exercise01_Zaehler.java
@@ -0,0 +1,17 @@
+package exercises;
+/**
+ * Aufgabenstellung:
+ * 
+ * Entwickle die Klasse "Zaehler" die als Instanzvariable einen int Wert enthält.
+ * Dieser kann mit der Methode hochzaehlen() um eins erhöht und mit zuruecksetzen() 
+ * auf 0 gesetzt werden.
+ * 
+ * Die Klasse soll folgende Funktionalitäten bieten:
+ * - Eine Instanzvariable vom Typ int zur Speicherung des aktuellen Zählerwerts
+ * - Einen Konstruktor, der den Zählerwert mit 0 initialisiert
+ * - Eine Methode hochzaehlen(), die den Zählerwert um 1 erhöht
+ * - Eine Methode zuruecksetzen(), die den Zählerwert auf 0 zurücksetzt
+ * - Eine Methode getWert(), die den aktuellen Zählerwert zurückgibt
+ * 
+ * Implementiere diese Klasse in einer neuen Datei namens "Zaehler.java".
+ */
